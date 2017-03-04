3c7b5489bb34bd38084ff64b1fef56e63257dc1c
pawelaugustyn <pawelaugustyn@protonmail.com>
2017-03-04 14:18:50 +0100

    Dokonczone generowanie losowych grafow dla przypadku G(n,l), do zrobienia G(n,p) oraz informacje o errorach

--- SimpleGraph/Model/GraphCreator.cs
+++ SimpleGraph/Model/GraphCreator.cs
@@ -29,23 +29,31 @@ namespace SimpleGraph.Model
 
         public static Graph CreateRandomGraph(int Nodes, int Connections)
         {
-            Graph randomGraph = new Graph();
-
-            for (int i = 0; i < Nodes; i++)
-                randomGraph.Nodes.Add(new Node() { ID = i });
 
 
             int maxConnections = Nodes * (Nodes - 1) / 2;
             Random rnd = new Random();
 
-            //Przypadek gdy polaczen do zrealizowania jest mniej niz maksymalna liczba polaczen
-            if (maxConnections - Connections > Connections)
+            if (Connections > maxConnections)
             {
+                //Tutaj wypisywanie bledu
+                //TODO
+                return new Graph();
+            }
+
+            //Przypadek gdy polaczen do zrealizowania jest mniej niz maksymalna liczba polaczen
+            if (maxConnections - Connections >= Connections)
+            {
+                Graph randomGraph = new Graph();
+
+                for (int i = 0; i < Nodes; i++)
+                    randomGraph.Nodes.Add(new Node() { ID = i });
                 while (Connections > 0)
                 {
                     int n1 = rnd.Next(0, Nodes);
                     int n2 = rnd.Next(0, Nodes);
-                    if (n1 == n2) continue;
+                    if (n1 == n2)
+                        continue;
                     if (randomGraph.Connections.Exists(x => x.Node1 == randomGraph.Nodes.Find(y => y.ID == n1) && x.Node2 == randomGraph.Nodes.Find(y => y.ID == n2)))
                         continue;
                     if (randomGraph.Connections.Exists(x => x.Node2 == randomGraph.Nodes.Find(y => y.ID == n1) && x.Node1 == randomGraph.Nodes.Find(y => y.ID == n2)))
@@ -58,9 +66,35 @@ namespace SimpleGraph.Model
 
                     Connections--;
                 }
+                return randomGraph;
             }
+            else
+            {
+                Graph randomGraph = CreateFullGraph(Nodes);
 
-            return randomGraph;
+                int toDelConnections = maxConnections - Connections;
+
+                while (toDelConnections > 0)
+                {
+                    int n1 = rnd.Next(0, Nodes);
+                    int n2 = rnd.Next(0, Nodes);
+                    if (n1 == n2)
+                        continue;
+                    Connection remove1 = randomGraph.Connections.Find(x => x.Node1 == randomGraph.Nodes.Find(y => y.ID == n1) && x.Node2 == randomGraph.Nodes.Find(y => y.ID == n2));
+                    Connection remove2 = randomGraph.Connections.Find(x => x.Node2 == randomGraph.Nodes.Find(y => y.ID == n1) && x.Node1 == randomGraph.Nodes.Find(y => y.ID == n2));
+                    Connection removeCompare = randomGraph.Connections.Find(x => x.Node1 == randomGraph.Nodes.Find(y => y.ID == Nodes));
+                    if (remove1 == removeCompare && remove2 == removeCompare)
+                        continue;
+                    if (remove1 != removeCompare)
+                        randomGraph.Connections.Remove(remove1);
+                    else
+                        randomGraph.Connections.Remove(remove2);
+
+                    toDelConnections--;
+                }
+
+                return randomGraph;
+            }
         }
 
         //Zerknijcie na tworzenie połączeń :D
@@ -75,20 +109,17 @@ namespace SimpleGraph.Model
             //Dodanie połączeń między wierzchołkami
             for (int i = 0; i < Nodes; i++)
             {
-                for (int j = 0; j < Nodes; j++)
+                for (int j = i+1; j < Nodes; j++)
                 {
-                    if (i != j)
-                    {
-                        Connection connection = new Connection();
-                        //Nie tworzymy nowych obiektów typu Node, tylko wyszukujemy w Nodes już istniejące - znacznie ułatwia
-                        //to rysowanie grafu - każdy obiekt Node dostaje później swoje współrzędne, ktore w obu listach są takie same.
-                        //Jeżeli utworzylibyśmy nowy obiekt to po dodaniu współrzędnych obiektom w Nodes, w Connection nie
-                        //zostałyby one zmienione.
-                        //(x => x.ID == i) jest to tzw. wyrażenie Lambda - w tym wypadku szukamy pierwszego elementu o ID równym i.
-                        connection.Node1 = fullGraph.Nodes.FirstOrDefault(x => x.ID == i);
-                        connection.Node2 = fullGraph.Nodes.FirstOrDefault(x => x.ID == j);
-                        fullGraph.Connections.Add(connection);
-                    }
+                    Connection connection = new Connection();
+                    //Nie tworzymy nowych obiektów typu Node, tylko wyszukujemy w Nodes już istniejące - znacznie ułatwia
+                    //to rysowanie grafu - każdy obiekt Node dostaje później swoje współrzędne, ktore w obu listach są takie same.
+                    //Jeżeli utworzylibyśmy nowy obiekt to po dodaniu współrzędnych obiektom w Nodes, w Connection nie
+                    //zostałyby one zmienione.
+                    //(x => x.ID == i) jest to tzw. wyrażenie Lambda - w tym wypadku szukamy pierwszego elementu o ID równym i.
+                    connection.Node1 = fullGraph.Nodes.FirstOrDefault(x => x.ID == i);
+                    connection.Node2 = fullGraph.Nodes.FirstOrDefault(x => x.ID == j);
+                    fullGraph.Connections.Add(connection);
                 }
             }          
 
